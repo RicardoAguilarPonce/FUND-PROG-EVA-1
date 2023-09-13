4620d22f4acb920d0cfb67964d832d979e11c9a8
Angel Ricardo Aguilar Ponce De León <142959411+RicardoAguilarPonce@users.noreply.github.com>
2023-09-13 16:55:10 -0700

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1nombre; nombre no válido, empieza con un número
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente
+        
+        //int public ;no podemos usar public, es palabra reservada (keyword)
+        int Public;
+        double aoifdasdp;
+    }
+    
+}
